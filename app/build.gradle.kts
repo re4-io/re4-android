d9e4986a22047aab7440bc9584ca7fd46e972394
Vladimir Berlev <1783633+tegoo@users.noreply.github.com>
2020-02-17 16:44:48 +0100

    Add androidx.navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.2.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.2.1")
     implementation("androidx.paging:paging-runtime-ktx:2.1.1")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
     implementation("com.google.android.material:material:1.0.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.3")
         classpath(kotlin("gradle-plugin", version = "1.3.61"))
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.1")
     }
 }
 
