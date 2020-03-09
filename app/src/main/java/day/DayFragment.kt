9e1158ff4232f02bf4998eeb813cd902c6d7fde1
Vladimir Berlev <1783633+tegoo@users.noreply.github.com>
2020-03-09 15:57:33 +0100

    Add DayFragment

---
+++ app/src/main/java/day/DayFragment.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020  Vladimir Berlev
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.re4.day
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import io.re4.R
+
+class DayFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.day_fragment, container, false)
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.timeline, menu)
+    }
+}
